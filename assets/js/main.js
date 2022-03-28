d7a12a836cdb62cead36c16983a55420a1fd68f2
Flo Lcs <florianlcs16@gmail.com>
2022-03-28 21:25:33 +0200

    home with js

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+//Navbar
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("#top-page nav ul li");
+let current = "";
+
+//Navbar
+window.onscroll = () => {
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.className === current) {
+      li.classList.add("active");
+    }
+  });
+};
