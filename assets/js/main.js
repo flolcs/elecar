9e18318f77393eabeb092fb9115f05845447a94b
FloLcs <florianlcs16@gmail.com>
2022-03-29 11:56:59 +0200

    home ok début du about

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("#top-page nav ul li");
 let current = "";
 
-//Navbar
 window.onscroll = () => {
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
@@ -18,3 +17,21 @@ window.onscroll = () => {
     }
   });
 };
+
+// Menu
+const toggleMenu = document.querySelector("#btn-menu");
+const toggleCloseMenu = document.querySelector("#btn-close");
+const sousMenu = document.querySelector("#sous-menu");
+console.log(toggleCloseMenu);
+
+toggleMenu.addEventListener("click", () => {
+  sousMenu.style.visibility = "visible";
+  toggleMenu.style.visibility = "hidden";
+  toggleCloseMenu.style.visibility = "visible";
+});
+
+toggleCloseMenu.addEventListener("click", () => {
+  sousMenu.style.visibility = "hidden";
+  toggleMenu.style.visibility = "visible";
+  toggleCloseMenu.style.visibility = "hidden";
+});
