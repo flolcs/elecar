0a166da8b3690e2556ca3a30b548d0921dd4cac7
FloLcs <florianlcs16@gmail.com>
2022-03-30 14:18:59 +0200

    home et about ok avec responsive et animation js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
+// Menu
+const toggleMenu = document.querySelector("#btn-menu");
+const toggleCloseMenu = document.querySelector("#btn-close");
+const sousMenu = document.querySelector("#sous-menu");
+
+toggleMenu.addEventListener("click", () => {
+  sousMenu.style.visibility = "visible";
+  toggleMenu.style.visibility = "hidden";
+  toggleCloseMenu.style.visibility = "visible";
+});
+
+toggleCloseMenu.addEventListener("click", () => {
+  sousMenu.style.visibility = "hidden";
+  toggleMenu.style.visibility = "visible";
+  toggleCloseMenu.style.visibility = "hidden";
+});
+
 //Navbar
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("#top-page nav ul li");
@@ -15,23 +32,40 @@ window.onscroll = () => {
     if (li.className === current) {
       li.classList.add("active");
     }
+    if (document.documentElement.clientWidth < 768) {
+      sousMenu.style.visibility = "hidden";
+      toggleCloseMenu.style.visibility = "hidden";
+      toggleMenu.style.visibility = "visible";
+    }
   });
 };
 
-// Menu
-const toggleMenu = document.querySelector("#btn-menu");
-const toggleCloseMenu = document.querySelector("#btn-close");
-const sousMenu = document.querySelector("#sous-menu");
-console.log(toggleCloseMenu);
-
-toggleMenu.addEventListener("click", () => {
-  sousMenu.style.visibility = "visible";
-  toggleMenu.style.visibility = "hidden";
-  toggleCloseMenu.style.visibility = "visible";
+// Animation
+const srTop = ScrollReveal({
+  origin: "top",
+  distance: "50px",
+  duration: 2000,
+  delay: 400,
+  //reset: true
 });
 
-toggleCloseMenu.addEventListener("click", () => {
-  sousMenu.style.visibility = "hidden";
-  toggleMenu.style.visibility = "visible";
-  toggleCloseMenu.style.visibility = "hidden";
+const srBot = ScrollReveal({
+  origin: "bottom",
+  distance: "50px",
+  duration: 2000,
+  delay: 400,
+  //reset: true
 });
+
+// Home animation
+
+srTop.reveal("h1");
+srTop.reveal("#home--divCarMission", { delay: 500 });
+srTop.reveal("#home--imgCar", { delay: 600 });
+srBot.reveal("#home--specifications", { delay: 700 });
+srBot.reveal("#home--start", { delay: 700 });
+
+// About Animation
+srTop.reveal("#about--container1", { delay: 500 });
+srTop.reveal("#about--container2", { delay: 500 });
+srBot.reveal("#about--container3", { delay: 750 });
